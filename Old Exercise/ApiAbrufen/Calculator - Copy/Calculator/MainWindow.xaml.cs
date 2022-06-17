db1278e0709fa8f267746fbf341a432ba24989b2
Julia <95243667+JuliaSorre@users.noreply.github.com>
2022-06-17 17:13:55 +0200

    Add files via upload

---
+++ Old Exercise/ApiAbrufen/Calculator - Copy/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        //variables
+        double num1;
+        double num2;
+        double result;
+        string clipresult;
+
+        //Class for the Clipboard
+
+        //public static void SetText(String textData);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Converts the strings to double variables and summs them
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+         
+            num1 = Convert.ToDouble(numerA.Text);
+            num2 = Convert.ToDouble(numerB.Text);
+            result = num1 + num2;
+            resultat.Content = result.ToString();
+            clipresult = result.ToString();
+            Clipboard.SetText(clipresult);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
