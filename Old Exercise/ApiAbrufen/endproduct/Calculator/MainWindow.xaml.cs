4bab77bc12e65a31c249b6bdc70f60f22324e86c
Julia <95243667+JuliaSorre@users.noreply.github.com>
2022-06-17 17:15:03 +0200

    Add files via upload

---
+++ Old Exercise/ApiAbrufen/endproduct/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    
+    public partial class MainWindow : Window
+    {
+        public Uri Source { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Web1.Navigate("https://www.wtv676.siemens-info.com/ev00000003/login.php");
+            
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Web2.Navigate("https://www.wtv676.siemens-info.com/ev16444187/login.php");
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            Web3.Navigate("https://www.wtv676.siemens-info.com/ev16444144/login.php");
+     
+        }
+
+        /*object var1 = GetValue("https://www.wtv676.siemens-info.com/ev16444144/api/.well-known/knx/");
+
+        object var2 = var1;*/
+
+   
+    }
+}
