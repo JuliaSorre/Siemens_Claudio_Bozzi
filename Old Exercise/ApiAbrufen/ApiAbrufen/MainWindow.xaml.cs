5c9c36110fd863d1a85a69f899599dce600b9cc1
Julia <95243667+JuliaSorre@users.noreply.github.com>
2022-06-17 17:12:33 +0200

    Old Exercises
    Those are some exercises I did to practice.

---
+++ Old Exercise/ApiAbrufen/ApiAbrufen/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net.Http;
+
+namespace apiresponse
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            /*
+             API:
+            https://www.wtv676.siemens-info.com/ev16444144/api/.well-known/knx/
+             */
+
+            HttpClient client = new HttpClient();
+
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                var response = await client.GetAsync("https://www.wtv676.siemens-info.com/ev16444144/api/.well-known/knx/");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+        }
+    }
+}
